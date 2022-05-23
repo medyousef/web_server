61e32e913a70e51db40766b574718e2396481a08
MohamedYoussef <medyousef95@gmail.com>
2022-05-23 17:16:59 +0200

    led states

--- web_server.py
+++ web_server.py
@@ -1,9 +1,15 @@
 from flask import Flask
 import RPi.GPIO as GPIO
+import time
+LED_LIST=[14,23,24]
 Button_PIN =18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Button_PIN, GPIO.IN)
 app=Flask(__name__)
+for led in LED_LIST:
+    GPIO.setup(led,GPIO.OUT)
+    GPIO.output(led, GPIO.LOW)
+
 
 #routes
 @app.route("/")
@@ -15,4 +21,21 @@ def push_button():
     if GPIO.input(Button_PIN)== GPIO.HIGH:
         return "Button is pressed"
     return "not pressed"
+@app.route("/led/<int:led_pin>/state/<int:led_state>")
+def trigger_led(led_pin,led_state):
+    if led_pin not in LED_LIST:
+        print("led no in the list")
+        if led_state==1:
+            state=GPIO.HIGH
+            mesg="on"
+        elif led_state==0:
+            state=GPIO.LOW
+            mesg="off"
+        else:
+            print("wrong state")
+    else:
+        GPIO.output(led_pin, state)
+        print("led : ", led_pin, "is :",mesg)
+
+    return
 app.run(host="0.0.0.0", port=5000)
