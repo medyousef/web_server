657a230842e272f3f51a6154f8ceaf74f7af71cb
MohamedYoussef <medyousef95@gmail.com>
2022-05-23 16:30:04 +0200

    new button

--- web_server.py
+++ web_server.py
@@ -1,8 +1,18 @@
 from flask import Flask
+import RPi.GPIO as GPIO
+Button_PIN =18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Button_PIN, GPIO.IN)
 app=Flask(__name__)
 
 #routes
 @app.route("/")
 def index():
     return "hello from flask"
-app.run(host="0.0.0.0")
+
+@app.route("/push-button")
+def push_button():
+    if GPIO.input(Button_PIN, GPIO.HIGH):
+        return "Button is pressed"
+    return "not pressed"
+app.run(host="0.0.0.0", port=5000)
